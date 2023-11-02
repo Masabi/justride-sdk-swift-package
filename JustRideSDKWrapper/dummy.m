8d70cef73f5b5dade037652f79cd2d3fad255516
Aleksandra Generowicz <62150130+ale-gen@users.noreply.github.com>
2023-11-02 10:17:53 +0100

    Delete JustRideSDKWrapper/dummy.m

--- JustRideSDKWrapper/dummy.m
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright 2023 Masabi Ltd
- */
-
-// A Swift Package Manager target needs at least one header to prevent a warning. See
-// https://github.com/firebase/firebase-ios-sdk/pull/6504.
