754dc4b18f44203be891cc435a9d5d5d4ee81af6
Native Platforms <native.platforms@masabi.com>
2025-10-17 14:30:03 +0200

    [Update] JustRideSDK (21.2.0)

--- Package.swift
+++ Package.swift
@@ -50,8 +50,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "JustRideSDK",
-            url: "https://sdk-artifactory.justride.com/artifactory/ios-artifacts/21.1.0/JustRideSDK.xcframework.zip",
-            checksum: "af696ea0acf179804af72e962f28fc8af8118706973ce36eb8ee15161a648bc6"
+            url: "https://sdk-artifactory.justride.com/artifactory/ios-artifacts/21.2.0/JustRideSDK.xcframework.zip",
+            checksum: "1615385f3f5596e64095fc01f8d34df6b02d70a85f2525f901b0683a1ee89da0"
         )
     ]
 )
